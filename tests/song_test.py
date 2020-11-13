508364f39eb944d77a330041b55569a483b789db
Michalju <juszkin@gmail.com>
2020-11-13 13:44:27 +0000

    Initial version

---
+++ tests/song_test.py
@@ -0,0 +1,8 @@
+import unittest
+from classes.guest import Guest
+from classes.room import Room
+from classes.song import Song
+
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        pass

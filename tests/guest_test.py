f57ff781b879480b53ebce5698abcacfc4059c20
Michalju <juszkin@gmail.com>
2020-11-13 14:52:20 +0000

    Adjusted self guests to be dictonary

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,17 +5,17 @@ from classes.song import Song
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guests = [
-            Guest("Donald Trump"),      # 0
-            Guest("Vladimir Putin"),    # 1
-            Guest("Kim Jong Un"),       # 2
-            Guest("James Hetfield"),    # 3
-            Guest("Clif Burton"),       # 4
-            Guest("Brian Scott"),       # 5
-            Guest("Angus Young")        # 6
-        ]
+        self.guests = {
+            "DonaldTrump" : Guest("Donald Trump"),         # 0
+            "VladimirPutin" : Guest("Vladimir Putin"),     # 1
+            "KimJongUn" : Guest("Kim Jong Un"),           # 2
+            "JamesHetfield" : Guest("James Hetfield"),     # 3
+            "ClifBurton" : Guest("Clif Burton"),           # 4
+            "BrianScott" : Guest("Brian Scott"),           # 5
+            "AngusYoung" : Guest("Angus Young")            # 6
+        }
     def test_room_name(self):
         # check that room name is created correctly
-        self.assertEqual(self.guests[0].name, "Donald Trump")
-        self.assertEqual(self.guests[1].name, "Vladimir Putin")
-        self.assertEqual(self.guests[5].name, "Brian Scott")  
+        self.assertEqual(self.guests["DonaldTrump"].name, "Donald Trump")
+        self.assertEqual(self.guests["VladimirPutin"].name, "Vladimir Putin")
+        self.assertEqual(self.guests["BrianScott"].name, "Brian Scott")  

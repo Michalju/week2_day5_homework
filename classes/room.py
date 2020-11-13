ec67fe41a389ef95ec2436633661bfc5288ed62e
Michalju <juszkin@gmail.com>
2020-11-13 15:38:38 +0000

    added check_out method to the class Room

--- classes/room.py
+++ classes/room.py
@@ -3,5 +3,24 @@ class Room:
         self.name = name
         self._guests = []
 
+    # This method return guest by name 
+    def find_guest_by_name(self,name):
+        for guest in self._guests:
+            if guest.name==name:
+                return guest
+
+
+    # This method checks in a guest to the room
     def check_in(self, guest):
         self._guests.append(guest)
+
+    # This method checks out from the room either:
+    # a single named person
+    # or
+    # all guests if name is ""
+    
+    def check_out(self, name):
+        if not name:
+            self._guests.clear()
+        else:
+            self._guests.remove(self.find_guest_by_name(name))
--- tests/room_test.py
+++ tests/room_test.py
@@ -27,4 +27,31 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(self.rooms["Shite"]._guests[0].name, "Donald Trump")
         self.assertEqual(self.rooms["Shite"]._guests[1].name, "Vladimir Putin")
         self.assertEqual(self.rooms["Rock&Roll"]._guests[0].name, "Angus Young")
-        self.assertEqual(self.rooms["Metal"]._guests[0].name, "James Hetfield")    
+        self.assertEqual(self.rooms["Metal"]._guests[0].name, "James Hetfield")   
+
+    def test_room_check_out(self):
+        # setting up the room with guests
+        self.rooms["Shite"].check_in(Guest("Donald Trump"))
+        self.rooms["Shite"].check_in(Guest("Vladimir Putin"))
+        self.rooms["Rock&Roll"].check_in(Guest("Angus Young"))
+        self.rooms["Rock&Roll"].check_in(Guest("Brian Scott"))
+        self.rooms["Metal"].check_in(Guest("James Hetfield"))
+        self.rooms["Metal"].check_in(Guest("Clif Burton"))
+
+        # checking out all from room Shite
+        self.assertEqual(len(self.rooms["Shite"]._guests), 2)
+        self.rooms["Shite"].check_out("")
+        self.assertEqual(len(self.rooms["Shite"]._guests), 0)   
+
+        # checking out Brian Scott from Rock & Roll
+        self.assertEqual(len(self.rooms["Rock&Roll"]._guests), 2)
+        self.rooms["Rock&Roll"].check_out("Angus Young")
+        self.assertEqual(len(self.rooms["Rock&Roll"]._guests), 1)
+        self.assertEqual(self.rooms["Rock&Roll"]._guests[0].name, "Brian Scott")        
+        
+        # checking out Clif Burton from Metal
+        self.assertEqual(len(self.rooms["Metal"]._guests), 2)
+        self.rooms["Metal"].check_out("Clif Burton")
+        self.assertEqual(len(self.rooms["Metal"]._guests), 1)
+        self.assertEqual(self.rooms["Metal"]._guests[0].name, "James Hetfield")
+

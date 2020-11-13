f55d9a2613164a683f068cab9f8cb0fc507e9ae5
Michalju <juszkin@gmail.com>
2020-11-13 14:16:03 +0000

    Added rooms

--- classes/room.py
+++ classes/room.py
@@ -1,3 +1,3 @@
 class Room:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
--- tests/room_test.py
+++ tests/room_test.py
@@ -5,4 +5,13 @@ from classes.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        pass
+        self.rooms = {
+            "Shite" : Room("Shite"),
+            "Rock&Roll" : Room("Rock&Roll"),
+            "Metal" : Room("Metal")}
+
+    def test_room_name(self):
+        # check that room name is created correctly
+        self.assertEqual(self.rooms["Shite"].name, "Shite")
+        self.assertEqual(self.rooms["Rock&Roll"].name, "Rock&Roll")
+        self.assertEqual(self.rooms["Metal"].name, "Metal")  

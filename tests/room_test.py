94662a69043224ee591b59cca8f7be33676bba89
Michalju <juszkin@gmail.com>
2020-11-15 14:35:31 +0000

    minor tidy up

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,42 +6,42 @@ from classes.song import Song
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.rooms = {
-            "Shite" : Room("Shite"),
+            "Pop" : Room("Pop"),
             "Rock&Roll" : Room("Rock&Roll"),
             "Metal" : Room("Metal")}
 
     def test_room_name(self):
         # check that room name is created correctly
-        self.assertEqual(self.rooms["Shite"].name, "Shite")
+        self.assertEqual(self.rooms["Pop"].name, "Pop")
         self.assertEqual(self.rooms["Rock&Roll"].name, "Rock&Roll")
         self.assertEqual(self.rooms["Metal"].name, "Metal")
 
     
     def test_room_check_in(self):
         # verify the check in function works
-        self.rooms["Shite"].check_in(Guest("Donald Trump"))
-        self.rooms["Shite"].check_in(Guest("Vladimir Putin"))
+        self.rooms["Pop"].check_in(Guest("Donald Trump"))
+        self.rooms["Pop"].check_in(Guest("Vladimir Putin"))
         self.rooms["Rock&Roll"].check_in(Guest("Angus Young"))
         self.rooms["Metal"].check_in(Guest("James Hetfield"))
 
-        self.assertEqual(self.rooms["Shite"]._guests[0].name, "Donald Trump")
-        self.assertEqual(self.rooms["Shite"]._guests[1].name, "Vladimir Putin")
+        self.assertEqual(self.rooms["Pop"]._guests[0].name, "Donald Trump")
+        self.assertEqual(self.rooms["Pop"]._guests[1].name, "Vladimir Putin")
         self.assertEqual(self.rooms["Rock&Roll"]._guests[0].name, "Angus Young")
         self.assertEqual(self.rooms["Metal"]._guests[0].name, "James Hetfield")   
 
     def test_room_check_out(self):
         # setting up the room with guests
-        self.rooms["Shite"].check_in(Guest("Donald Trump"))
-        self.rooms["Shite"].check_in(Guest("Vladimir Putin"))
+        self.rooms["Pop"].check_in(Guest("Donald Trump"))
+        self.rooms["Pop"].check_in(Guest("Vladimir Putin"))
         self.rooms["Rock&Roll"].check_in(Guest("Angus Young"))
         self.rooms["Rock&Roll"].check_in(Guest("Brian Scott"))
         self.rooms["Metal"].check_in(Guest("James Hetfield"))
         self.rooms["Metal"].check_in(Guest("Clif Burton"))
 
-        # checking out all from room Shite
-        self.assertEqual(len(self.rooms["Shite"]._guests), 2)
-        self.rooms["Shite"].check_out("")
-        self.assertEqual(len(self.rooms["Shite"]._guests), 0)   
+        # checking out all from room Pop
+        self.assertEqual(len(self.rooms["Pop"]._guests), 2)
+        self.rooms["Pop"].check_out("")
+        self.assertEqual(len(self.rooms["Pop"]._guests), 0)   
 
         # checking out Brian Scott from Rock & Roll
         self.assertEqual(len(self.rooms["Rock&Roll"]._guests), 2)
@@ -57,12 +57,12 @@ class TestRoom(unittest.TestCase):
 
     def test_room_add_songs(self):
         # adding songs to a room
-        self.assertEqual(len(self.rooms["Shite"]._songs), 0) 
+        self.assertEqual(len(self.rooms["Pop"]._songs), 0) 
         self.assertEqual(len(self.rooms["Rock&Roll"]._songs), 0) 
         self.assertEqual(len(self.rooms["Metal"]._songs), 0) 
 
-        self.rooms["Shite"].add_song(Song("Nothing But Thieves", "Particles"))
-        self.rooms["Shite"].add_song(Song("Nothing But Thieves", "Amsterdam"))
+        self.rooms["Pop"].add_song(Song("Nothing But Thieves", "Particles"))
+        self.rooms["Pop"].add_song(Song("Nothing But Thieves", "Amsterdam"))
 
         self.rooms["Rock&Roll"].add_song(Song("AC/DC", "Highway To Hell"))
 
@@ -70,6 +70,6 @@ class TestRoom(unittest.TestCase):
         self.rooms["Metal"].add_song(Song("Metallica", "Seek And Destroy"))
         self.rooms["Metal"].add_song(Song("Slipknot", "Unsainted"))
 
-        self.assertEqual(len(self.rooms["Shite"]._songs), 2) 
+        self.assertEqual(len(self.rooms["Pop"]._songs), 2) 
         self.assertEqual(len(self.rooms["Rock&Roll"]._songs), 1) 
         self.assertEqual(len(self.rooms["Metal"]._songs), 3)             
